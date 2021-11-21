67b23a89ccf415178caa6a7f1683e95089b5c782
polinaprinii <91224252+polinaprinii@users.noreply.github.com>
2021-11-21 17:33:06 +0000

    Updated main.py & Journal

--- Vaccine_Analysis_by_Country/Main Project/main.py
+++ Vaccine_Analysis_by_Country/Main Project/main.py
@@ -2,12 +2,11 @@
 
 import pandas as pd
 
-pd. set_option('display.max_rows', 500)
-
 # Next we move to creating variables which will hold our three dataset.
 # Using the read_csv() function we can directly specify a path to the csv file in the git repository using the "raw" view.
 
 # Calling our first dataset "vaccinatios.csv"
 
 df_1 = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv')
-print(df_1)
+print(df_1, '\n')  # perform check to see if the above
+print(df_1.columns.values)  # we check if
