66c7dd31634fd9976bd116d88f7c35980a735726
polinaprinii <91224252+polinaprinii@users.noreply.github.com>
2021-11-27 16:07:02 +0000

    Update MongoDB_Import.py
    Code cleaned up, and for loop as well as try catches introduced.

--- Vaccine_Analysis_by_Country/Main Project/MongoDB_Import.py
+++ Vaccine_Analysis_by_Country/Main Project/MongoDB_Import.py
@@ -1,14 +1,17 @@
 # Importing necessary packages to allows us to push JSON files into MongoDB
 
-import json                             # JSON package as we are importing JSON files into MongoDB
+import json  # JSON package as we are importing JSON files into MongoDB
 
 import pymongo.errors
-from pymongo import MongoClient         # Pymongo package which provides tools for working with MongoDB
-                                        # MongoClient tool to connect to a MongoDB
+from pymongo import MongoClient  # Pymongo package which provides tools for working with MongoDB
+
+MONGODB_IP = 'mongodb://192.168.56.30:27017'
+
+# MongoClient tool to connect to a MongoDB
 
 # Setting up our connection:
 try:
-    my_DB = MongoClient('mongodb://192.168.56.30:27017')
+    my_DB = MongoClient(MONGODB_IP)
     print('Ping successful: ', my_DB.admin.command('ping'))
 
 except pymongo.errors.ServerSelectionTimeoutError as Error1:
@@ -20,36 +23,46 @@ except Exception as Error2:
 # Declaring our database which will hold our collections aka tables.
 db = my_DB['Vaccinations_DB']
 
-# Creating our collections aka tables for each JSON file
-Collection1 = db['df_1']
-Collection2 = db['df_2']
-Collection3 = db['df_3']
+collections = [db['df_1'], db['df_2'], db['df_3']]  # List of collections in MongoDB
 
 # Loading the JSON files into the declared collections.
 # Loading df_1
-with open('D:\Git\DAP-Project\Vaccine_Analysis_by_Country\Main Project\JSON Files\df_1.json') as file1:
-    file1_data = json.load(file1)
+filePath_1 = 'D:\Git\DAP-Project\Vaccine_Analysis_by_Country\Main Project\JSON Files\df_1.json'
+filePath_2 = 'D:\Git\DAP-Project\Vaccine_Analysis_by_Country\Main Project\JSON Files\df_2.json'
+filePath_3 = 'D:\Git\DAP-Project\Vaccine_Analysis_by_Country\Main Project\JSON Files\df_3.json'
 
-#Loading df_2
-with open('D:\Git\DAP-Project\Vaccine_Analysis_by_Country\Main Project\JSON Files\df_2.json') as file2:
-    file2_data = json.load(file2)
+filePaths = [filePath_1, filePath_2, filePath_3]
 
-# Loading df_3
-with open('D:\Git\DAP-Project\Vaccine_Analysis_by_Country\Main Project\JSON Files\df_3.json') as file3:
-    file3_data = json.load(file3)
+# A function to ensure we only get one import of the JSON file into our declared collections.
+def clear_collections(collections):
+    for collection in collections:
+        if collection.count() > 0:
+            collection.drop()
 
-if isinstance(file1_data, list):
-    Collection1.insert_many(file1_data)
-else:
-    Collection1.insert_one(file1_data)
+clear_collections(collections)
 
-if isinstance(file2_data, list):
-    Collection2.insert_many(file2_data)
-else:
-    Collection2.insert_one(file2_data)
+# Populates collections in MongoDB
+def populate_collections(collections, filePaths):
 
-if isinstance(file3_data, list):
-    Collection3.insert_many(file3_data)
-else:
-    Collection3.insert_one(file3_data)
+    try:
+        """
+        Loop through filePaths and enumerate them to give them an index. Enumerate gives index top items in list.
+        Open and load JSOn file.
+        Populate collections with JSON file.
+        """
+        for index, filepath in enumerate(filePaths):
+            with open(filepath) as f:
+                file_data = json.load(f)
 
+            if isinstance(file_data, list):
+                collections[index].insert_many(file_data)
+            else:
+                collections[index].insert_one(file_data)
+
+    except pymongo.errors.DuplicateKeyError as Error2:
+        print('There seems to be an error when inserting the data', Error2)
+
+    except pymongo.errors.WriteError as Error3:
+        print('There seems to be an error when writing the data', Error3)
+        
+populate_collections(collections, filePaths)
