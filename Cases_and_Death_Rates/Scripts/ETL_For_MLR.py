699f7a3e8f15d2bae7fb5504a7754a67f6cc2887
alunprice <x21123659@student.ncirl.ie>
2021-12-07 17:45:02 +0000

    used groupby and sum to reduce data set for MLR to only one instance of each country

--- Cases_and_Death_Rates/Scripts/ETL_For_MLR.py
+++ Cases_and_Death_Rates/Scripts/ETL_For_MLR.py
@@ -59,6 +59,8 @@ dataframes_to_concat = [df1, df2]
 
 all_cases_and_deaths = pd.concat(dataframes_to_concat, ignore_index=True)
 
+all_cases_and_deaths=all_cases_and_deaths.groupby(['isocode', 'country']).sum().reset_index()
+
 # noinspection PyTypeChecker
 all_cases_and_deaths.to_csv(r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Data for Multiple Linear '
                             r'Regression\Global_and_US_Confirmed_Cases_And_Deaths.csv')
