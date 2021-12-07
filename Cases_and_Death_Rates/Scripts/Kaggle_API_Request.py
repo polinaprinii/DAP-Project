142a524c5066d85aae62ed5f5ad19a4bc29f4284
alunprice <x21123659@student.ncirl.ie>
2021-12-07 19:40:35 +0000

    Update Kaggle_API_Request.py

--- Cases_and_Death_Rates/Scripts/Kaggle_API_Request.py
+++ Cases_and_Death_Rates/Scripts/Kaggle_API_Request.py
@@ -43,15 +43,19 @@ with zipfile.ZipFile('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data
                      'r') as zipref:
     zipref.extractall(r"A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA")
 
-df1 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_global_confirmed_cases.csv', dtype= 'unicode')
-df2 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_global_deaths.csv', dtype= 'unicode')
-df3 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_us_confirmed_cases.csv', dtype= 'unicode')
-df4 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_us_deaths.csv', dtype= 'unicode')
+df1 = pd.read_csv(
+    'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_global_confirmed_cases.csv',
+    dtype='unicode')
+df2 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_global_deaths.csv',
+                  dtype='unicode')
+df3 = pd.read_csv(
+    'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_us_confirmed_cases.csv',
+    dtype='unicode')
+df4 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_us_deaths.csv',
+                  dtype='unicode')
 
 frames = [df1, df2, df3, df4]
 for frame in frames:
     print(frame.head())
-
-
 
 ########################################################################################################################
