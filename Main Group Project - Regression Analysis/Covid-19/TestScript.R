a7ccb3983a73a1abb595bf6b83e2d33e76ae1c0a
polinaprinii <91224252+polinaprinii@users.noreply.github.com>
2021-12-08 20:30:49 +0000

    Update TestScript.R

--- Main Group Project - Regression Analysis/Covid-19/TestScript.R
+++ Main Group Project - Regression Analysis/Covid-19/TestScript.R
@@ -1,3 +1,7 @@
 test <- 'Hello'
 print(test)
 
+print('Trying to get Github to display the .R files in the language bar.')
+print('Think it may be due to the fact that the percentage behind the 
+language is too little to display')
+print("Adding more code to see if that helps.")
