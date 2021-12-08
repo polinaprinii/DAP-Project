e252595cc1256471828104ea9132c6257bb04262
polinaprinii <91224252+polinaprinii@users.noreply.github.com>
2021-12-08 20:35:05 +0000

    Update TestScript.R

--- Main Group Project - Regression Analysis/Covid-19/TestScript.R
+++ Main Group Project - Regression Analysis/Covid-19/TestScript.R
@@ -5,3 +5,8 @@ print('Trying to get Github to display the .R files in the language bar.')
 print('Think it may be due to the fact that the percentage behind the 
 language is too little to display')
 print("Adding more code to see if that helps.")
+print('Think my theory is corrct, adding a csv file to further
+      expand the volume of the sript file.')
+
+csvfile <- read.csv(file = 'D:/Git/DAP-Project/Vaccine_Analysis_by_Country/Main Project/CSV Files/finalexport.csv')
+head(csvfile)
