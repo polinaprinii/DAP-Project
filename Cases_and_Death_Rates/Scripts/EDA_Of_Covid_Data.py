31f9ad4a008d4b8ca3cb65a0e8b25124da1ff6de
alunprice <x21123659@student.ncirl.ie>
2021-12-13 18:17:13 +0000

    Edit of Data Pipeline
    Addition of Automation using Master script to import and run other scripts.

--- Cases_and_Death_Rates/Scripts/EDA_Of_Covid_Data.py
+++ Cases_and_Death_Rates/Scripts/EDA_Of_Covid_Data.py
@@ -4,44 +4,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from IPython.display import clear_output
+import pymongo
 
 
 # This file will handle the exploratory data analysis of the two global cases and death files from mongoDB
 
 # Read in from MONGODB
+# Connecting to MongoDB
+client = pymongo.MongoClient('192.168.56.30', 27017)
 
+# Mongo Database where the cleaned data is stored
+db = client.covid_data
 
-#
-# df1 = pd.read_csv(
-#     'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_confirmed_Cases.csv')
-# df2 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_deaths.csv')
-# df3 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Confirmed_Cases.csv')
-# df4 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Deaths.csv')
-#
-# df1 = df1.drop(['Unnamed: 0'], axis=1)
-# print(df1.columns)
+# Database Collection Names
+covid_cases_clean = db.covid_cases_clean
+covid_deaths_clean = db.covid_deaths_clean
 
-###Plot global cases in a bar chart
-########################################################################################################################
-# set iso as index
+# Pulling records from Mongodb to dataframes
+df1 = pd.DataFrame(list(covid_cases_clean.find()))
+df2 = pd.DataFrame(list(covid_deaths_clean.find()))
+
+df1.drop(['_id'], axis=1, inplace=True)
+df2.drop(['_id'], axis=1, inplace=True)
+
+# Plot global cases in a bar chart
+# Create List of date Values
 date_list = df1.columns.tolist()[11:]
 print(date_list[0], date_list[-1])
 
-df1_1 = df1.groupby('Country/Region').sum()[date_list]
-df2_2 = df2.groupby('Country/Region').sum([date_list])
-print(df1_1[df1_1['1/29/20'] > 0])
 
-###top 10 countries with the highest numbers of confirmed cases Globally.
+# Top 10 countries with the highest numbers of confirmed cases Globally.
 
-print('The top 10 Countries in Cases to Date are: ',
+print('The top 10 Countries in Cases to Date are: \n',
       df1[['Country/Region', date_list[-1]]].sort_values(by=date_list[-1], ascending=False).head(10))
 
-###top 10 countries with the highest number of deaths Globally.
+# Top 10 countries with the highest number of deaths Globally.
 
 print('The top 10 Countries in Deaths to Date are: ',
       df2[['Country/Region', date_list[-1]]].sort_values(by=date_list[-1], ascending=False).head(10))
 
-### Creating Bar Charts to show the top 20 countries in Cases for Each Date
+# Creating Bar Charts to show the top 20 countries in Cases for Each Date
 # time.sleep(3)
 # for d in date_list:
 #     clear_output(wait=True)
@@ -79,3 +81,4 @@ plt.xlabel('Dates (01/29/20 - 12/07/21)')
 plt.ylabel('Total Global Confirmed Cases to Date')
 plt.title('Total Global Confirmed Cases By Date')
 plt.show()
+
---
+++ Cases_and_Death_Rates/Scripts/Master.py
@@ -0,0 +1,3 @@
+import GitHub_Data_Request
+import Read_Dataset_Write_To_Database
+import Dataset_Transformation
