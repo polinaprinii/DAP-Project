6667024e932c766eb78bf4589a95af6d7f83544c
polinaprinii <91224252+polinaprinii@users.noreply.github.com>
2021-12-13 21:02:27 +0000

    Create Master.py
    Master file to hold all code in one file.

---
+++ Vaccine_Analysis_by_Country/Main Project/Master.py
@@ -0,0 +1,13 @@
+# Importing all files into one Master.py file to allows for seamless running pf the code in one go.
+
+import Extract_Raw_Data
+
+import MongoDB_Import
+
+import MongoDb_Transformations
+
+import PostgreSQL_ImportTransform
+
+import PostgreSQLExport
+
+import Data_Visualisation
