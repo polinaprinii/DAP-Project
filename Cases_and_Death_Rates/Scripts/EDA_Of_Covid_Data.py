9e480e61d1ed6893cd7fe499af89a8e11fbe819e
alunprice <x21123659@student.ncirl.ie>
2021-12-13 08:44:53 +0000

    Updated Data pipeline

--- Cases_and_Death_Rates/Scripts/EDA_Of_Covid_Data.py
+++ Cases_and_Death_Rates/Scripts/EDA_Of_Covid_Data.py
@@ -1,56 +1,81 @@
-import plotly.graph_objects as go
-import numpy as np
-from plotly.subplots import make_subplots
+import io
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from IPython.display import clear_output
 
 
-data = pd.read_csv(
-    'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_global_confirmed_cases.csv',
-    dtype='unicode')
+# This file will handle the exploratory data analysis of the two global cases and death files from mongoDB
 
-data2 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\CONVENIENT_global_deaths.csv',
-                  dtype='unicode')
+# Read in from MONGODB
 
 
-data.dropna(axis=0,inplace=True)
-data2.dropna(axis=0,inplace=True)
+#
+# df1 = pd.read_csv(
+#     'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_confirmed_Cases.csv')
+# df2 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_deaths.csv')
+# df3 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Confirmed_Cases.csv')
+# df4 = pd.read_csv('A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Deaths.csv')
+#
+# df1 = df1.drop(['Unnamed: 0'], axis=1)
+# print(df1.columns)
 
-data['Country/Region']=pd.to_datetime(data['Country/Region'])
-data2['Country/Region']=pd.to_datetime(data2['Country/Region'])
+###Plot global cases in a bar chart
+########################################################################################################################
+# set iso as index
+date_list = df1.columns.tolist()[11:]
+print(date_list[0], date_list[-1])
 
-# Create figure
-fig = go.Figure()
+df1_1 = df1.groupby('Country/Region').sum()[date_list]
+df2_2 = df2.groupby('Country/Region').sum([date_list])
+print(df1_1[df1_1['1/29/20'] > 0])
 
-# Add traces, one for each slider step
-for step in data.columns.values[1:]:
-    fig.add_trace(
-        go.Scatter(
-            visible=True,
-            line=dict(width=2),
-            name="Confirmed cases in " + step,
-            x=data['Country/Region'],
-            y=data[step].values,
-            marker=dict(color=[i for i in range(len(data2.columns.values[1:]))])))
+###top 10 countries with the highest numbers of confirmed cases Globally.
 
-steps = []
-for i in range(len(fig.data)):
-    step = dict(
-        method="update",
-        args=[{"visible": [False] * len(fig.data)},
-              {"title": "Slider switched to country: " + data.columns.values[1:][i]}],  # layout attribute
-    )
-    step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
-    steps.append(step)
-sliders = [dict(
-    active=100,
-    currentvalue={"prefix": "Frequency: "},
-    steps=steps
-)]
+print('The top 10 Countries in Cases to Date are: ',
+      df1[['Country/Region', date_list[-1]]].sort_values(by=date_list[-1], ascending=False).head(10))
 
-fig.update_layout(
-    title_text="Change The Slider To Change To Different Countries",
+###top 10 countries with the highest number of deaths Globally.
 
-    sliders=sliders
-)
+print('The top 10 Countries in Deaths to Date are: ',
+      df2[['Country/Region', date_list[-1]]].sort_values(by=date_list[-1], ascending=False).head(10))
 
-fig.show()
+### Creating Bar Charts to show the top 20 countries in Cases for Each Date
+# time.sleep(3)
+# for d in date_list:
+#     clear_output(wait=True)
+#     top_20_per_d = df1.groupby('Country/Region')[['Country/Region', d]].sum().sort_values(by=d, ascending=False).head(20)
+#     top_20_per_d.plot(kind='barh', log=True, figsize=(8,6))
+#     plt.ylabel("Country/Region", labelpad=14)
+#     plt.xlabel("# of Confirmed Cases (log=True)", labelpad=14)
+#     plt.title("Chart the confirmed cases per country/region", y=1.02)
+#     plt.show()
+#     time.sleep(1)
+
+### Bar Graph showing Cumalative Confirmed Cases By Date For The United States
+
+dates = df1.columns.tolist()[11:]
+cases_to_date = df1.iloc[255,11:]
+plt.bar(dates, cases_to_date)
+plt.xticks(dates, size=8)
+plt.locator_params(axis='x', nbins=9)
+plt.xlabel('Dates (01/29/20 - 12/07/21)')
+plt.ylabel('Confirmed Cases to Date')
+plt.title('Cases To Date for United States')
+
+plt.show()
+
+
+### Bar Graph to show total Confirmed Cases Worldwide by Date, To Date
+# Adding a row to sum daily global values.
+df1 = df1.append(df1.sum(numeric_only=True), ignore_index=True)
+totals = df1.iloc[-1,11:]
+
+plt.bar(dates, totals)
+plt.xticks(dates, size=8)
+plt.locator_params(axis='x', nbins=9)
+plt.xlabel('Dates (01/29/20 - 12/07/21)')
+plt.ylabel('Total Global Confirmed Cases to Date')
+plt.title('Total Global Confirmed Cases By Date')
+plt.show()
--- Cases_and_Death_Rates/Scripts/test github grab.py
+++ Cases_and_Death_Rates/Scripts/test github grab.py
@@ -2,17 +2,24 @@
 import pandas as pd
 import pycountry
 import requests.exceptions
+import country_converter as coco
 
 pd.options.mode.chained_assignment = None
 
 ########################################################################################################################
-
+# This script will download all RAW CSV files from
+# https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
+#I will then Save Them Locally
 # Assigning the correct RAW github Urls to Variables
 
-url1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-url2 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-url3 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
-url4 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+url1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/" \
+       "csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+url2 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/" \
+       "csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+url3 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/" \
+       "csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+url4 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/" \
+       "csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 all_urls = [url1, url2, url3, url4]
 # Reading the content of the URL
 
@@ -43,103 +50,25 @@ except requests.exceptions.RequestException as erreq:
     print("Misc Error", erreq)
 
 else:
-    # Ensuring each dataframe now contains data and letting user know that the process was successful.
+    # Checking that the dataframes are not empty, printing success notice.
     if df1.size and df2.size and df3.size and df4.size > 0:
         print("data successfully imported and stored in dataframe.")
 
-    # Printing the first 5 rows of each dataframe
+# Printing the first 5 rows of each dataframe.
+
 finally:
     all_dfs = [df1, df2, df3, df4]
     for i in all_dfs:
         print(i.head(5))
 
+# Saving the CSV files locally
 
-########################################################################################################################
-
-def CountryISOConverter(dataframe):
-    countries = {}
-    for country in pycountry.countries:
-        countries[country.name] = country.alpha_3
-
-    codes = [countries.get(country, 'ISO Code Unknown') for country in dataframe['Country/Region']]
-
-    dataframe['iso3'] = codes
-    cols1 = dataframe.columns.tolist()
-    cols1 = cols1[-1:] + cols1[:-1]
-    dataframe = dataframe[cols1]
-    return dataframe
-
-
-df1 = CountryISOConverter(df1)
-df2 = CountryISOConverter(df2)
-
-
-# print(df2.head(), '\n', df1.head(), '\n')
-
-
-########################################################################################################################
-
-
-def Combined_Key_Col_Add(dataframe):
-    dataframe["Combination_Key"] = dataframe['Country/Region'].str.cat(dataframe['Province/State'], sep=", ")
-
-    Combined_Key_Col = dataframe.pop("Combination_Key")
-
-    dataframe.insert(3, "Combination_Key", Combined_Key_Col)
-
-    return dataframe
-
-
-df1 = Combined_Key_Col_Add(df1)
-df2 = Combined_Key_Col_Add(df2)
-
-
-########################################################################################################################
-def Null_Count(dataframe):
-    no_nulls = (dataframe.isnull().sum())
-
-    return print(no_nulls)
-
-
-def Province_fill(dataframe):
-    dataframe['Province/State'].fillna(dataframe['Country/Region'], inplace=True)
-
-    return dataframe
-
-
-def Combination_fill(dataframe):
-    dataframe['Combination_Key'].fillna(dataframe['Country/Region'] + ", Province not Known", inplace=True)
-
-    return dataframe
-
-
-print("Null Count of Dataframe 1 = \n", Null_Count(df1))
-print("Null Count of Dataframe 2 = \n", Null_Count(df2))
-
-df1 = Province_fill(df1)
-df2 = Province_fill(df2)
-
-df1 = Combination_fill(df1)
-
-########################################################################################################################
-
-print("new dataframe = \n", df1.head(), "new dataframe = \n", df2.head())
-
-########################################################################################################################
-
-df1 = df1.rename(columns={'Country/Region': 'country'})
-df1 = df1.rename(columns={'iso3': 'isocode'})
-df2 = df2.rename(columns={'Country/Region': 'country'})
-df2 = df2.rename(columns={'iso3': 'isocode'})
-
-
-
-df.drop(['Lat', 'Long'], axis=1, inplace=True)
-
-
-# df1.to_json("who_global_confirmed_cases.json", orient='records')
-df1.to_csv("A:\College\DAP-Project\Cases_and_Death_Rates\Data\who_global_confirmed_Cases.csv")
-df2.to_csv("A:\College\DAP-Project\Cases_and_Death_Rates\Data\who_global_deaths.csv")
-# df3.to_csv("who_US_Deaths.csv")
-# df4.to_csv("who_US_Confirmed_Cases.csv")
+df1.to_csv(
+    r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_confirmed_Cases.csv')
+df2.to_csv(
+    r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_deaths.csv')
+df3.to_csv(
+    r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Deaths.csv')
+df4.to_csv(
+    r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Confirmed_Cases.csv')
 
