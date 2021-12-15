30b85605c35005495bea1d20b0f66883186d417d
polinaprinii <x21137757@student.ncirl.ie>
2021-12-15 00:32:48 +0000

    Update Data_Visualisation.py
    Finalised approach to the best way to display the data via plotly.
    Will tie it all it tomorrow.

--- Vaccine_Analysis_by_Country/Main Project/Data_Visualisation.py
+++ Vaccine_Analysis_by_Country/Main Project/Data_Visualisation.py
@@ -10,29 +10,39 @@ df = pd.read_csv("/Users/polinaprinii/Documents/GitHub/DAP-Project/Vaccine_Analy
 # Checking if read was successful.
 print(df.head())
 
-# # Plot horizontal bar chart
-# plot = px.data.tips()
-# fig = px.bar(df, x="totalvaccinations", y="country", title="Total Vaccinations Administered by Country",
-#              color="fullyvacnumber", labels=dict(totalvaccinations="Total Vaccinations (2 dose)", country="Country",
-#                                                  fullyvacnumber="Number of Fully Vaccinated"), orientation='h')
-# fig.update_layout(barmode='stack', yaxis={'categoryorder':'total ascending'})
-# fig.update_xaxes(type="log")
-# fig.show()
+# Split the data into smaller sub dataframes to
+lowsub = df[df["totalvaccinations"] < 10000000]
 
-# TODO: Figure out why not all country display on the y axis and make easier to read.
+midsub = df[(df["totalvaccinations"] > 10000000) & (df["totalvaccinations"] < 100000000)]
+
+highsub = df[df["totalvaccinations"] > 100000000]
+
+
+# Plot horizontal bar chart
+plot = px.data.tips()
+fig = px.bar(lowsub, x="totalvaccinations", y="country", title="Total Vaccinations Administered by Country",
+             color="fullyvacnumber", text="totalvaccinations", labels=dict(totalvaccinations="Total Vaccinations (2 dose)", country="Country",
+                                                 fullyvacnumber="Number of Fully Vaccinated", ), orientation='h')
+fig.update_layout(barmode='stack', yaxis={'categoryorder':'total ascending'})
+fig.update_xaxes(type="log")
+fig.show()
+
+# TODO: Increase font of the text feature in the bar chart as well as group other sub dataframes into the bar chart.
 
 
 # Attempting to built a Choropleth Map:
 
 import plotly.express as px
 
-df = px.choropleth(df,
+worldMap = px.choropleth(lowsub,
                 title="World Map of Fully Vaccinated",
                 locations ="isocode",
                 color="fullyvacnumber",
                 hover_name="country", # column to add to hover information
                 color_continuous_scale=px.colors.sequential.Viridis,)
-df.update_layout(
+worldMap.update_layout(
     coloraxis_colorbar=dict(
         title="Number of Fully Vaccinated"))
-df.show()
+worldMap.show()
+
+# TODO: Group all three sub datasets into one aka, 3 pages of same graph.
