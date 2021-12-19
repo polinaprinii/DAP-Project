5f643e7cc9e9f0385e6efc7b934fc67ce9df3363
alunprice <x21123659@student.ncirl.ie>
2021-12-19 02:31:21 +0000

    Edited Data pipeline.
    made adjustments to covid EDA and visualizations

--- Cases_and_Death_Rates/Scripts/Read_Dataset_Write_To_Database.py
+++ Cases_and_Death_Rates/Scripts/Read_Dataset_Write_To_Database.py
@@ -6,13 +6,13 @@ import pprint
 # Reading RAW files from local storage
 try:
     Raw_global_cases = pd.read_csv(
-        'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_confirmed_Cases.csv')
+        r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_confirmed_Cases.csv')
     Raw_global_deaths = pd.read_csv(
-        'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_deaths.csv')
+        r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_global_deaths.csv')
     Raw_US_cases = pd.read_csv(
-        'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Confirmed_Cases.csv')
+        r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Confirmed_Cases.csv')
     Raw_US_deaths = pd.read_csv(
-        'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Deaths.csv')
+        r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\RAW_who_US_Deaths.csv')
 
 except FileNotFoundError:
     print("File not found.")
@@ -20,8 +20,6 @@ except pd.errors.EmptyDataError:
     print("No data")
 except pd.errors.ParserError:
     print("Parse error")
-except Exception:
-    print("Some other exception")
 else:
     print("File Successfully Read")
 
@@ -30,13 +28,11 @@ finally:
     for df in all_dfs:
         print(df.head())
 
-
 # Converting CSV files to Dictionaries
 Raw_global_cases = Raw_global_cases.to_dict('records')
 Raw_global_deaths = Raw_global_deaths.to_dict('records')
 Raw_US_deaths = Raw_US_deaths.to_dict('records')
 Raw_US_cases = Raw_US_cases.to_dict('records')
-
 
 # Connecting to MongoDB
 client = pymongo.MongoClient('192.168.56.30', 27017)
@@ -55,13 +51,9 @@ Cases_US = covid_cases_US.insert_many(Raw_US_cases)
 Deaths_Glob = covid_deaths_global.insert_many(Raw_global_deaths)
 Deaths_US = covid_deaths_US.insert_many(Raw_US_deaths)
 
-
-
 df1 = pd.DataFrame(list(covid_cases_global.find()))
 df2 = pd.DataFrame(list(covid_cases_US.find()))
 df3 = pd.DataFrame(list(covid_deaths_global.find()))
 df4 = pd.DataFrame(list(covid_deaths_US.find()))
 
-
-#df1.to_csv(r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\TEST_FROM_MONGODB.csv')
-
+# df1.to_csv(r'A:\College\DAP-Project\Cases_and_Death_Rates\Data\Raw Data for EDA\TEST_FROM_MONGODB.csv')
