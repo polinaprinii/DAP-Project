110cdb7b92915960b2433c67e71bd944380afaff
MariaMigrova <78366923+MariaMigrova@users.noreply.github.com>
2021-12-21 17:48:08 +0000

    Update Ireland Slovakia stringency.py

--- Master Project/Restriction_and_Testing_Analysis/Mini_Analysis/RESTRICTIONS/Ireland Slovakia stringency.py
+++ Master Project/Restriction_and_Testing_Analysis/Mini_Analysis/RESTRICTIONS/Ireland Slovakia stringency.py
@@ -3,21 +3,21 @@
 
 # In[21]:
 
-
+#Libraries
 import pandas as pd
 import plotly.graph_objects as go
 
 
 # In[24]:
 
-
+#Reading the file
 IrelandSlovakia = pd.read_csv("D:\Git\DAP-Project\Restriction and Testing Analysis\Mini Analysis\RESTRICTIONS\IrelandSlovakiastringency.csv")
 IrelandSlovakia
 
 
 # In[37]:
 
-
+#Creating the plot
 fig = px.area(
     IrelandSlovakia, 
     x = 'Day',
@@ -35,7 +35,7 @@ fig
 
 # In[40]:
 
-
+#Saving the plot
 fig.write_image("D:\Git\DAP-Project\Restriction and Testing Analysis\Mini Analysis\RESTRICTIONS\IrelandSlovakia.png")
 
 
