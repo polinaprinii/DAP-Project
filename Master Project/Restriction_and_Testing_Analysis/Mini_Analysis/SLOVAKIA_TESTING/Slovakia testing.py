b9a2a306d20d9ceb6ff968a1d19dc2a75b25d749
MariaMigrova <78366923+MariaMigrova@users.noreply.github.com>
2021-12-21 17:49:16 +0000

    Update Slovakia testing.py

--- Master Project/Restriction_and_Testing_Analysis/Mini_Analysis/SLOVAKIA_TESTING/Slovakia testing.py
+++ Master Project/Restriction_and_Testing_Analysis/Mini_Analysis/SLOVAKIA_TESTING/Slovakia testing.py
@@ -3,7 +3,7 @@
 
 # In[84]:
 
-
+#Libraries
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
@@ -13,25 +13,25 @@ import numpy
 # In[55]:
 
 
-
+#Reading the file
 SlovakiaTesting=pd.read_csv("D:\Git\DAP-Project\Restriction and Testing Analysis\Mini Analysis\SLOVAKIA TESTING\SlovakiaTesting.csv",error_bad_lines=False, engine="python")
 
 
 # In[65]:
 
-
+#Removing NA values
 SlovakiaTesting = SlovakiaTesting.dropna()
 
 
 # In[125]:
 
-
+#Creating the plot
 fig = px.bar(SlovakiaTesting, x="Gender", y="PCRPos", color="Gender",facet_col="AgeGroup",template="plotly_white")
 
 
 # In[ ]:
 
-
+#Saving the plot
 fig.write_html("D:\Git\DAP-Project\Restriction and Testing Analysis\Mini Analysis\SLOVAKIA TESTING\SlovakiaTesting.html")
 
 
